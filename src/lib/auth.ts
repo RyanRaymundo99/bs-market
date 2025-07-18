e1a6efc81b798c68d1f98012622c74695f6d4241
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2025-07-17 18:13:50 -0700

    trying to fix deployment issue

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,8 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@prisma/client";
 import { sendEmail } from "@/lib/email";
 import { nextCookies } from "better-auth/next-js";
-
-const prisma = new PrismaClient();
+import prisma from "@/lib/prisma";
 
 
 
